3a97d93d9b53ba1baa89ec637097a5caadab9e37
waelhe <143899717+waelhe@users.noreply.github.com>
2026-05-15 19:47:13 +0300

    fix: add missing assertThrows import in DisputeServiceTest

--- marketplace-disputes/src/test/java/com/marketplace/disputes/DisputeServiceTest.java
+++ marketplace-disputes/src/test/java/com/marketplace/disputes/DisputeServiceTest.java
@@ -16,6 +16,7 @@ import static org.instancio.Instancio.*;
 import static org.instancio.Select.field;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 class DisputeServiceTest {
